a4836d2b63d3851d770d3bbcbd15883f05874155
Sarju Mulmi <mulmi.sarju@gmail.com>
2018-09-17 11:59:32 -0400

    cache setup to read from data.json file

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 import express from 'express';
-
+import bodyparser from 'body-parser';
+import morgan from 'morgan';
+import fs from 'fs';
+import path from 'path';
 
 const app = express();
 const router = express.Router();
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('combined'));
+}
+app.use(bodyparser.json());
+app.use(bodyparser.json({type: 'application/json'}));
+app.use(bodyparser.urlencoded({extended:true}))
+
+let dataPath = path.join(__dirname, 'data.json')
+let rawdata = fs.readFileSync(dataPath, 'utf8');
+let cache = JSON.parse(rawdata);
 
 router.get('/', (req, res) => {
-  res.json({hello: 'world'})
+  res.json(cache)
 });
+
+// router.post('/add', (req, res) => {
+//   for (const key in req.body) {
+//     if (req.body.hasOwnProperty(key)) {
+//       cache[key] = req.body[key]; 
+//     }
+//   }
+//   fs.writeFileSync(dataPath, JSON.stringify(cache));
+//   res.json(cache);
+// })
 
 app.use(router);
 
